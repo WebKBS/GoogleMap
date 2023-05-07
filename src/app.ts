df36eda0b02e926b54dd29b067133a8661d93166
BeomSu-Kang <kbs21c1987@gmail.com>
2023-05-07 20:50:24 +0900

    구글맵 타입스크립트로 사용하기

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,43 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+const GOOGLE_API_KEY = "";
+
+//declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // 구글 send API
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      console.log(response);
+      if (response.data.status !== "OK") {
+        throw new Error("fetch 에러");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 8,
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
